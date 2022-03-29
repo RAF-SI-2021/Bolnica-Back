d5339f95c69c8b855a68c7920b83919693be8f77
unknown <mrakic17@raf.rs>
2022-03-29 22:32:19 +0200

    added entities in managment service

---
+++ management/src/main/java/raf/si/bolnica/management/entities/Operacija.java
@@ -0,0 +1,73 @@
+package raf.si.bolnica.management.entities;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+public class Operacija {
+
+    @Id
+    @Column(name = "operacija_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long operacijaId;
+    @Column(nullable = false)
+    private Date datumOperacije;
+    @Column(nullable = false)
+    private String opis;
+    private Boolean obrisan = false;
+
+    // FKs
+    @ManyToOne
+    @JoinColumn(name = "zdravstveni_karton_id", nullable = false)
+    private ZdravstveniKarton zdravstveniKarton;
+    @Column(nullable = false)
+    private long pbo;
+
+    public long getOperacijaId() {
+        return operacijaId;
+    }
+
+    public void setOperacijaId(long operacijaId) {
+        this.operacijaId = operacijaId;
+    }
+
+    public long getPbo() {
+        return pbo;
+    }
+
+    public void setPbo(long pbo) {
+        this.pbo = pbo;
+    }
+
+    public Date getDatumOperacije() {
+        return datumOperacije;
+    }
+
+    public void setDatumOperacije(Date datumOperacije) {
+        this.datumOperacije = datumOperacije;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public Boolean getObrisan() {
+        return obrisan;
+    }
+
+    public void setObrisan(Boolean obrisan) {
+        this.obrisan = obrisan;
+    }
+
+    public ZdravstveniKarton getZdravstveniKarton() {
+        return zdravstveniKarton;
+    }
+
+    public void setZdravstveniKarton(ZdravstveniKarton zdravstveniKarton) {
+        this.zdravstveniKarton = zdravstveniKarton;
+    }
+}
