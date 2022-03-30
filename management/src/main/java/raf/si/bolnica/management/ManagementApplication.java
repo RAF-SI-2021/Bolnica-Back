365b4822cc9526b9aca525bde1149b8847e252a3
Luka Petrovic <petrovic.luka99@gmail.com>
2022-03-30 19:07:19 +0200

    Integrated Swagger to User and Management services

--- management/src/main/java/raf/si/bolnica/management/ManagementApplication.java
+++ management/src/main/java/raf/si/bolnica/management/ManagementApplication.java
@@ -1,13 +1,19 @@
 package raf.si.bolnica.management;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableAutoConfiguration(exclude = {
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class})
 public class ManagementApplication {
 
 	public static void main(String[] args) {
