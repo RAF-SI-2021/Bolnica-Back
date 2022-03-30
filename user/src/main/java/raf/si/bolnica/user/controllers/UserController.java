f39da320ef49b64198a496c401a28eee9f35bbcc
Pavle98 <pavlevukovic98@gmail.com>
2022-03-30 03:08:22 +0200

    Forgot Password functionality done

--- user/src/main/java/raf/si/bolnica/user/controllers/UserController.java
+++ user/src/main/java/raf/si/bolnica/user/controllers/UserController.java
@@ -1,11 +1,14 @@
 package raf.si.bolnica.user.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import raf.si.bolnica.user.interceptors.LoggedInUser;
 import raf.si.bolnica.user.models.User;
 import raf.si.bolnica.user.service.UserService;
+import raf.si.bolnica.user.service.EmailService;
+
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -19,6 +22,9 @@ public class UserController {
     @Autowired
     private LoggedInUser loggedInUser;
 
+    @Autowired
+    private EmailService emailService;
+
     @GetMapping(value = "/fetch-user/{username}")
     public ResponseEntity<User> fetchAdminByUsername(@PathVariable("username") String username) {
         return ok(userService.fetchUserByEmail(username));
@@ -28,5 +34,25 @@ public class UserController {
     public String hello() {
         System.out.println(loggedInUser.getRoles());
         return "Hello " + loggedInUser.getUsername();
+    }
+
+    @PostMapping("/forgot-password")
+    public HttpStatus forgotPassword(@RequestBody String usersEmail) {
+
+        User user = userService.fetchUserByEmail(usersEmail);
+
+        System.out.println("ovo je username : " + usersEmail);
+
+        if (user == null) {
+            //username doesnt exist, return 403
+            return HttpStatus.valueOf(403);
+        } else {
+            //username exists, proceeds to generate and save new password, also send email
+            String generatedPassword = userService.generateNewPassword(user);
+            userService.savePassword(user, generatedPassword);
+            emailService.sendEmail(usersEmail, generatedPassword);
+            return HttpStatus.valueOf(200);
+        }
+
     }
 }
--- user/src/main/java/raf/si/bolnica/user/service/UserService.java
+++ user/src/main/java/raf/si/bolnica/user/service/UserService.java
@@ -5,4 +5,6 @@ import raf.si.bolnica.user.models.User;
 public interface UserService {
 
     User fetchUserByEmail(String email);
+    String generateNewPassword(User user);
+    void savePassword(User user, String password);
 }
