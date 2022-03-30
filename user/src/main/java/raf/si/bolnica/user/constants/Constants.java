0bd64d6e8f8800b9268d62569c476f1577e62268
Miloš Maksimović <milosmilos@ymail.com>
2022-03-30 13:49:30 +0200

    Added User Exception Handler for Registration

--- user/src/main/java/raf/si/bolnica/user/constants/Constants.java
+++ user/src/main/java/raf/si/bolnica/user/constants/Constants.java
@@ -4,12 +4,30 @@ public class Constants {
 
     public static final String BASE_API = "/api";
     public static final String LOGIN = "/login";
+    public static final String CREATE_EMPLOYEE = "/create-employee";
 
     public static final String JWT_KEY = "QnuQblQWn8H9ggiwfGbCxpPA3gdY1oAe";
 
     public interface ForgetPassword {
         String DEVELOPER_MESSAGE = "Password didn't match with the original one.";
         String MESSAGE = "Incorrect password.Forgot Password?";
+    }
+
+    public interface RegistrationInvalidFields {
+        String DEVELOPER_MESSAGE_USERNAME_INVALID_MIN_CHARACTERS = "Username not valid";
+        String MESSAGE_USERNAME_INVALID_MIN_CHARACTERS = "Min 5 characters";
+
+        String DEVELOPER_MESSAGE_USERNAME_INVALID_MAX_CHARACTERS = "Username not valid";
+        String MESSAGE_USERNAME_INVALID_MAX_CHARACTERS = "Max 30 characters";
+
+        String DEVELOPER_MESSAGE_USERNAME_INVALID_FORMAT = "Username not valid";
+        String MESSAGE_USERNAME_INVALID_FORMAT = "Only digits and characters";
+
+        String DEVELOPER_MESSAGE_USER_TITLE_INVALID = "User title not valid";
+        String MESSAGE_USER_TITLE_INVALID = "There is no such title";
+
+        String DEVELOPER_MESSAGE_USER_PROFESSION_INVALID = "User profession not valid";
+        String MESSAGE_USER_PROFESSION_INVALID = "There is no such profession";
     }
 
     public interface InvalidAdminUsername {
---
+++ user/src/main/java/raf/si/bolnica/user/exceptionHandler/user/UserExceptionHandler.java
@@ -0,0 +1,54 @@
+package raf.si.bolnica.user.exceptionHandler.user;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Component;
+import raf.si.bolnica.user.constants.Constants;
+import raf.si.bolnica.user.exceptions.InvalidRegistrationException;
+import raf.si.bolnica.user.exceptions.UnauthorisedException;
+import raf.si.bolnica.user.models.User;
+import raf.si.bolnica.user.requests.LoginRequestDTO;
+import raf.si.bolnica.user.utils.FileUtils;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+@Component
+public class UserExceptionHandler {
+
+    public final Consumer<String> validateUsername = (username) -> {
+        String pattern = "[a-zA-Z0-9]+";
+        if (!username.matches(pattern)) {
+            throw new InvalidRegistrationException(Constants.RegistrationInvalidFields.MESSAGE_USERNAME_INVALID_FORMAT, Constants.RegistrationInvalidFields.DEVELOPER_MESSAGE_USERNAME_INVALID_FORMAT);
+        }
+        if (username.length() < 5)
+            throw new InvalidRegistrationException(Constants.RegistrationInvalidFields.MESSAGE_USERNAME_INVALID_MIN_CHARACTERS, Constants.RegistrationInvalidFields.DEVELOPER_MESSAGE_USERNAME_INVALID_MIN_CHARACTERS);
+        if (username.length() > 30)
+            throw new InvalidRegistrationException(Constants.RegistrationInvalidFields.MESSAGE_USERNAME_INVALID_MAX_CHARACTERS, Constants.RegistrationInvalidFields.DEVELOPER_MESSAGE_USERNAME_INVALID_MAX_CHARACTERS);
+
+    };
+
+    public final Consumer<String> validateUserTitle = (userTitle) -> {
+        List<String> titles = FileUtils.readUserTitles();
+        if (!titles.contains(userTitle))
+            throw new InvalidRegistrationException(Constants.RegistrationInvalidFields.MESSAGE_USER_TITLE_INVALID, Constants.RegistrationInvalidFields.DEVELOPER_MESSAGE_USER_TITLE_INVALID);
+    };
+
+    public final Consumer<String> validateUserProfession = (userProfession) -> {
+        List<String> professions = FileUtils.readUserProfessions();
+        if (!professions.contains(userProfession))
+            throw new InvalidRegistrationException(Constants.RegistrationInvalidFields.MESSAGE_USER_PROFESSION_INVALID, Constants.RegistrationInvalidFields.DEVELOPER_MESSAGE_USER_PROFESSION_INVALID);
+    };
+
+    public final Consumer<User> validateUserUsername = (admin) -> {
+        if (Objects.isNull(admin))
+            throw new UnauthorisedException(Constants.InvalidAdminUsername.MESSAGE, Constants.InvalidAdminUsername.DEVELOPER_MESSAGE);
+    };
+
+    public final BiConsumer<LoginRequestDTO, User> validatePassword = (requestDTO, admin) -> {
+        if (!BCrypt.checkpw(requestDTO.getPassword(), admin.getPassword())) {
+            throw new UnauthorisedException(Constants.ForgetPassword.MESSAGE, Constants.ForgetPassword.DEVELOPER_MESSAGE);
+        }
+    };
+
+}
