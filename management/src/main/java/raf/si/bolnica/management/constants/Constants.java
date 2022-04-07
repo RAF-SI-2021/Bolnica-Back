0e1aa3154a62f11a569d53f4942efb811a7028bb
aleksamilisavljevic <aleksamilis@gmail.com>
2022-04-07 19:11:15 +0200

    Code quality ispravke part 2

--- management/src/main/java/raf/si/bolnica/management/constants/Constants.java
+++ management/src/main/java/raf/si/bolnica/management/constants/Constants.java
@@ -1,6 +1,9 @@
 package raf.si.bolnica.management.constants;
 
 public class Constants {
+
+    private Constants() { }
+
     public static final String VISA_MED_SESTRA = "ROLE_VISA_MED_SESTRA";
     public static final String MED_SESTRA = "ROLE_MED_SESTRA";
     public static final String ADMIN = "ROLE_ADMIN";
