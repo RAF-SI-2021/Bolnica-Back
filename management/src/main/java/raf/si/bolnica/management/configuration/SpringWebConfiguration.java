f1233944140b5bdd04a5acfe599397a6f257ca70
Luka Petrovic <petrovic.luka99@gmail.com>
2022-04-08 22:19:39 +0200

    Moved Swagger configuration to SpringWebConfiguration

--- management/src/main/java/raf/si/bolnica/management/configuration/SpringWebConfiguration.java
+++ management/src/main/java/raf/si/bolnica/management/configuration/SpringWebConfiguration.java
@@ -3,15 +3,23 @@ package raf.si.bolnica.management.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import raf.si.bolnica.management.interceptors.LoggedInUser;
 import raf.si.bolnica.management.interceptors.UserInfoInterceptor;
 import raf.si.bolnica.management.jwt.JwtProperties;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Base64;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
+@EnableSwagger2
 public class SpringWebConfiguration extends WebMvcConfigurationSupport {
 
     @Autowired
@@ -24,5 +32,23 @@ public class SpringWebConfiguration extends WebMvcConfigurationSupport {
     public void addInterceptors(InterceptorRegistry registry) {
         String secretKey = Base64.getEncoder().encodeToString(jwtProperties.getSecretKey().getBytes());
         registry.addInterceptor(new UserInfoInterceptor(loggedInUser, secretKey));
+    }
+
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("guru.springframework"))
+                .paths(regex("/api/v1/*"))
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
