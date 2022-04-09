e860d63a0680bd8752fd5118c013e049ceccccac
Miloš Maksimović <milosmilos@ymail.com>
2022-04-09 16:37:15 +0200

    Added exception handler

---
+++ management/src/main/java/raf/si/bolnica/management/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package raf.si.bolnica.management.exceptionHandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import raf.si.bolnica.management.exceptions.AllergenNotExistException;
+import raf.si.bolnica.management.exceptions.ErrorResponse;
+import raf.si.bolnica.management.exceptions.MissingRequestFieldsException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = MissingRequestFieldsException.class)
+    public ResponseEntity<ErrorResponse> handleMissingRequestFieldsException(MissingRequestFieldsException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>(e.getErrorResponse(), e.getErrorResponse().getResponseStatus());
+    }
+
+    @ExceptionHandler(value = AllergenNotExistException.class)
+    public ResponseEntity<ErrorResponse> handleAllergenAlreadyExist(AllergenNotExistException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>(e.getErrorResponse(), e.getErrorResponse().getResponseStatus());
+    }
+}
