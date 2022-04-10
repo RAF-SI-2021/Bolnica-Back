f9fc3274159769941ada51ef6fa4f3f27a6eac68
Djordje <djolecekic@gmail.com>
2022-04-10 03:44:12 +0200

    Implemented search for appointments by lbz and date

--- management/src/main/java/raf/si/bolnica/management/repositories/ScheduledAppointmentRepository.java
+++ management/src/main/java/raf/si/bolnica/management/repositories/ScheduledAppointmentRepository.java
@@ -4,8 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import raf.si.bolnica.management.entities.ZakazaniPregled;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 
 @Repository
 public interface ScheduledAppointmentRepository extends JpaRepository<ZakazaniPregled, Long> {
             ZakazaniPregled getZakazaniPregledByZakazaniPregledId(long id);
+            List<ZakazaniPregled> findByLBZLekaraAndAndDatumIVremePregleda(long lBZLekara, Timestamp datumIVremePregleda);
+            List<ZakazaniPregled> findByLBZLekara(long lBZLekara);
+
 }
--- management/src/main/java/raf/si/bolnica/management/service/ScheduledAppointmentService.java
+++ management/src/main/java/raf/si/bolnica/management/service/ScheduledAppointmentService.java
@@ -6,10 +6,15 @@ import raf.si.bolnica.management.requests.CreateScheduledAppointmentRequestDTO;
 import raf.si.bolnica.management.requests.UpdateAppointmentStatusDTO;
 import raf.si.bolnica.management.requests.UpdateArrivalStatusDTO;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 
 public interface ScheduledAppointmentService {
 
         ZakazaniPregled setAppointment(CreateScheduledAppointmentRequestDTO createScheduledAppointmentRequestDTO);
         ZakazaniPregled updateAppointment(UpdateAppointmentStatusDTO updateAppointmentStatusDTO);
         ZakazaniPregled updateArrival(UpdateArrivalStatusDTO updateArrivalStatusDTO);
+        List<ZakazaniPregled> getAppointmentByLBZ(long lbz) ;
+        List<ZakazaniPregled> getAppointmentByLBZAndDate(long lbz, Timestamp date);
 }
