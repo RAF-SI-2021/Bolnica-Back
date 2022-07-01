3b11d5c81db43cff0d0e71557649b8e9654c790e
dsejat <dsejat@girddynamics.com>
2022-07-01 13:45:12 +0200

    Values fix

--- management/src/main/java/raf/si/bolnica/management/requests/CreateOtpusnaListaDTOValidator.java
+++ management/src/main/java/raf/si/bolnica/management/requests/CreateOtpusnaListaDTOValidator.java
@@ -8,14 +8,10 @@ public class CreateOtpusnaListaDTOValidator {
 
     public static String validate(CreateOtpusnaListaDTO req) {
 
-        if (req.getPbo() == 0) return "Pbo je obavezno polje";
+        if (req.getPbo() == -1) return "Pbo je obavezno polje";
         if (req.getLbp() == null) return "Lbp je obavezno polje";
-        if (req.getPrateceDijagnoze() == null) return "Pratece dijagnoze su obavezno polje";
         if (req.getAnamneza() == null) return "Anamneza je obavezno polje";
-        if (req.getAnalize() == null) return "Analize je obavezno polje";
-        if (req.getTokBolesti() == null) return "Tok bolesti je obavezno polje";
         if (req.getZakljucak() == null) return "Zakljucak je obavezno polje";
-        if (req.getTerapija() == null) return "Terapija je obavezno polje";
         return "OK";
     }
 
