1c06db3412536d095ced6102463f9991f10de64f
Aleksa Basic <basicaleksa1@gmail.com>
2022-07-03 23:36:26 +0200

    Removed eureka from pom.xml

--- management/src/main/java/raf/si/bolnica/management/ManagementApplication.java
+++ management/src/main/java/raf/si/bolnica/management/ManagementApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 @EnableFeignClients
 @EnableAutoConfiguration(exclude = {
         SecurityAutoConfiguration.class,
