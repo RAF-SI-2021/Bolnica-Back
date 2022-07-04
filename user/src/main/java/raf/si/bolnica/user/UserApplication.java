fb60330b6b9489c6aefe3ea84577f23b4938aac2
Luka Petrovic <petrovic.luka99@gmail.com>
2022-07-04 09:24:43 +0200

    Switched to Docker local

--- user/src/main/java/raf/si/bolnica/user/UserApplication.java
+++ user/src/main/java/raf/si/bolnica/user/UserApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.Managemen
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableAutoConfiguration(exclude = {
 		SecurityAutoConfiguration.class,
